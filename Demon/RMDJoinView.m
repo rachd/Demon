a3030a2d31465522fe80e8075691f61fba3b7e3b
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-09-20 08:34:36 -0400

    add name field and label to join view

--- Demon/RMDJoinView.m
+++ Demon/RMDJoinView.m
@@ -15,28 +15,67 @@
     if (self) {
         self.backgroundColor = [UIColor colorWithRed:0.05 green:0.00 blue:0.00 alpha:1.0];
         
-        self.closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [self.closeButton setTitle:@"X" forState:UIControlStateNormal];
-        self.closeButton.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.closeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self addSubview:self.closeButton];
-        
-        NSDictionary *viewsDictionary = @{@"closeButton":self.closeButton};
-        NSArray *constraint_POS_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[closeButton(100)]"
-                                                                            options:0
-                                                                            metrics:nil
-                                                                              views:viewsDictionary];
-        
-        NSArray *constraint_POS_H = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[closeButton(100)]"
-                                                                            options:0
-                                                                            metrics:nil
-                                                                              views:viewsDictionary];
-        
-        [self addConstraints:constraint_POS_V];
-        [self addConstraints:constraint_POS_H];
-        
+        [self setUpTextField];
+        [self setUpCloseButton];
     }
     return self;
+}
+
+- (void)setUpTextField {
+    UITextField *nameField = [[UITextField alloc] init];
+    nameField.translatesAutoresizingMaskIntoConstraints = NO;
+    nameField.placeholder = @"Placeholder";
+    nameField.textColor = [UIColor blackColor];
+    nameField.backgroundColor = [UIColor whiteColor];
+    nameField.layer.cornerRadius = 6;
+    [self addSubview:nameField];
+    
+    UILabel *nameLabel = [[UILabel alloc] init];
+    nameLabel.text = @"Your Name: ";
+    nameLabel.textColor = [UIColor whiteColor];
+    nameLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    nameLabel.font = [UIFont systemFontOfSize:16];
+    [self addSubview:nameLabel];
+    
+    NSDictionary *viewDictionary = @{@"nameField":nameField, @"nameLabel":nameLabel};
+    NSArray *constraints_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-80-[nameLabel(40)]-[nameField(50)]"
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:viewDictionary];
+    NSArray *constraints_H_label = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[nameLabel]-30-|"
+                                                                           options:0
+                                                                           metrics:nil
+                                                                             views:viewDictionary];
+    NSArray *constraints_H_field = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[nameField]-30-|"
+                                                                           options:0
+                                                                           metrics:nil
+                                                                             views:viewDictionary];
+    [self addConstraints:constraints_V];
+    [self addConstraints:constraints_H_label];
+    [self addConstraints:constraints_H_field];
+}
+
+- (void)setUpCloseButton {
+    self.closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.closeButton setTitle:@"X" forState:UIControlStateNormal];
+    self.closeButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.closeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self addSubview:self.closeButton];
+    
+    NSDictionary *viewsDictionary = @{@"closeButton":self.closeButton};
+    NSArray *constraint_POS_V = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[closeButton(100)]"
+                                                                        options:0
+                                                                        metrics:nil
+                                                                          views:viewsDictionary];
+    
+    NSArray *constraint_POS_H = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[closeButton(100)]"
+                                                                        options:0
+                                                                        metrics:nil
+                                                                          views:viewsDictionary];
+    
+    [self addConstraints:constraint_POS_V];
+    [self addConstraints:constraint_POS_H];
+
 }
 
 @end
