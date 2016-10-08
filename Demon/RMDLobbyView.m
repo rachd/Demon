e0f0054f90d955e705c4dee8b5020e443fe04edb
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-10-07 21:22:16 -0400

    change lobby image and colors

--- Demon/RMDLobbyView.m
+++ Demon/RMDLobbyView.m
@@ -19,16 +19,17 @@
 - (instancetype)init {
     self = [super init];
     if (self) {
-        self.backgroundColor = [UIColor colorWithRed:0.05 green:0.00 blue:0.00 alpha:1.0];
+        self.backgroundColor = [UIColor colorWithRed:0.64 green:0.11 blue:0.11 alpha:1.0];//[UIColor colorWithRed:0.05 green:0.00 blue:0.00 alpha:1.0];
     
         self.demonImage = [[UIImageView alloc] init];
-        self.demonImage.image = [UIImage imageNamed:@"Demon"];
+        self.demonImage.image = [UIImage imageNamed:@"DemonSVG"];
         self.demonImage.translatesAutoresizingMaskIntoConstraints = NO;
         [self addSubview:self.demonImage];
     
         self.hostButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [self.hostButton setTitle:@"Host a Game" forState:UIControlStateNormal];
-        self.hostButton.layer.borderColor = [[UIColor whiteColor] CGColor];
+        [self.hostButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        self.hostButton.layer.borderColor = [[UIColor blackColor] CGColor];
         self.hostButton.layer.borderWidth = 4;
         self.hostButton.layer.cornerRadius = 12;
         self.hostButton.translatesAutoresizingMaskIntoConstraints = NO;
@@ -36,7 +37,8 @@
     
         self.joinButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [self.joinButton setTitle:@"Join a Game" forState:UIControlStateNormal];
-        self.joinButton.layer.borderColor = [[UIColor whiteColor] CGColor];
+        self.joinButton.layer.borderColor = [[UIColor blackColor] CGColor];
+        [self.joinButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         self.joinButton.layer.borderWidth = 4;
         self.joinButton.layer.cornerRadius = 12;
         self.joinButton.translatesAutoresizingMaskIntoConstraints = NO;
@@ -61,7 +63,7 @@
         titleLabel.text = @"Demon";
         titleLabel.font = [UIFont systemFontOfSize:40];
         titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        titleLabel.textColor = [UIColor whiteColor];
+        titleLabel.textColor = [UIColor blackColor];
         [self addSubview:titleLabel];
         
         UIStackView *titleStack = [[UIStackView alloc] initWithArrangedSubviews:@[titleLabel, self.mainStack]];
